d502a3edf42fb011d57aaa8b179f64a4dfccdca4
lzxprogrammer <lzxprogrammer@outlook.com>
2018-12-04 15:04:48 +0800

    测试csrf

--- app/Http/Middleware/EnableCrossRequestMiddleware.php
+++ app/Http/Middleware/EnableCrossRequestMiddleware.php
@@ -15,19 +15,19 @@ class EnableCrossRequestMiddleware
      */
     public function handle($request, Closure $next)
     {
-        $response = $next($request);
-//        $origin = $request->server('HTTP_ORIGIN') ? $request->server('HTTP_ORIGIN') : '';
-//        $allow_origin = [
-//            '*',
-//        ];
-//        if (in_array($origin, $allow_origin)) {
-//            $response->header('Access-Control-Allow-Origin', $origin);
+        // $response = $next($request);
+        // $origin = $request->server('HTTP_ORIGIN') ? $request->server('HTTP_ORIGIN') : '';
+        // $allow_origin = [
+        //     '*',
+        // ];
+        // if (in_array($origin, $allow_origin)) {
+            // $response->header('Access-Control-Allow-Origin', $origin);
             $response->header('Access-Control-Allow-Origin', '*');
             $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, X-CSRF-TOKEN, X-Requested-With, Accept, Authorization, X-XSRF-TOKEN');
             //$response->header('Access-Control-Expose-Headers', 'Authorization, authenticated');
             $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
             $response->header('Access-Control-Allow-Credentials', 'false');
-//        }
+        // }
         return $next($request);
     }
 }
