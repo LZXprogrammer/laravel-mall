0794e66418117536ecbc3330eecf71329032b766
lzxprogrammer <lzxprogrammer@outlook.com>
2018-12-04 15:23:41 +0800

    测试csrf

--- app/Http/Middleware/EnableCrossRequestMiddleware.php
+++ app/Http/Middleware/EnableCrossRequestMiddleware.php
@@ -25,7 +25,7 @@ class EnableCrossRequestMiddleware
             $response->header('Access-Control-Allow-Origin', '*');
             $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, X-CSRF-TOKEN, X-Requested-With, Accept, Authorization, X-XSRF-TOKEN');
             //$response->header('Access-Control-Expose-Headers', 'Authorization, authenticated');
-            $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
+            $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT');
             $response->header('Access-Control-Allow-Credentials', 'false');
         // }
         return $next($request);
