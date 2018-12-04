a8116c287eb4fedad7633e0f305abf2d5f1b5d25
lzxprogrammer <lzxprogrammer@outlook.com>
2018-12-04 15:51:12 +0800

    解决跨域问题

--- app/Http/Middleware/EnableCrossRequestMiddleware.php
+++ app/Http/Middleware/EnableCrossRequestMiddleware.php
@@ -13,37 +13,33 @@ class EnableCrossRequestMiddleware
      * @param  \Closure  $next
      * @return mixed
      */
-    // public function handle($request, Closure $next)
-    // {
-    //     $response = $next($request);
-    //     // $origin = $request->server('HTTP_ORIGIN') ? $request->server('HTTP_ORIGIN') : '';
-    //     // $allow_origin = [
-    //     //     '*',
-    //     // ];
-    //     // if (in_array($origin, $allow_origin)) {
-    //         // $response->header('Access-Control-Allow-Origin', $origin);
-    //         $response->header('Access-Control-Allow-Origin', '*');
-    //         $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, X-CSRF-TOKEN, X-Requested-With, Accept, Authorization, X-XSRF-TOKEN');
-    //         //$response->header('Access-Control-Expose-Headers', 'Authorization, authenticated');
-    //         $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT');
-    //         $response->header('Access-Control-Allow-Credentials', 'false');
-    //     // }
-    //     return $next($request);
-    // }
-
     public function handle($request, Closure $next)
     {
         $response = $next($request);
-        $response->header('Access-Control-Allow-Headers', 'Origin, application/x-www-form-urlencoded, Content-Type, Cookie, X-CSRF-TOKEN, Authorization, Accept');
-        // response allow return Authorization header
-        $response->header('Access-Control-Expose-Headers', 'Authorization');
-        $response->header('Access-Control-Allow-Origin', '*');
-        $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-        $response->header('Access-Control-Max-Age', '3628800');
 
-        if ($request->server('REQUEST_METHOD') == 'OPTIONS') {
-            $response->header('status', '200 ok');
-        }
+        // $origin = $request->server('HTTP_ORIGIN') ? $request->server('HTTP_ORIGIN') : '';
+        // $allow_origin = [
+            // 这里写允许的域名列表
+            // eg: 'http://www.baidu.com',
+        // ];
+        // if (in_array($origin, $allow_origin)) {
+            // $response->header('Access-Control-Allow-Origin', $origin);
+            
+            $response->header('Access-Control-Allow-Origin', '*');
+            $response->header('Access-Control-Allow-Headers', 'Origin, application/x-www-form-urlencoded, Content-Type, Cookie, X-CSRF-TOKEN, Authorization, Accept');
+            // response allow return Authorization header
+            $response->header('Access-Control-Expose-Headers', 'Authorization');
+            $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+            $response->header('Access-Control-Max-Age', '3628800');
+
+            // 如果下面为 true 时，'Access-Control-Allow-Origin' 的值不能为 * ，只能是请求域名
+            // $response->header('Access-Control-Allow-Credentials', 'false');
+
+            if ($request->server('REQUEST_METHOD') == 'OPTIONS') {
+                $response->header('status', '200 ok');
+            }
+
+        // }
 
         return $response;
     }
