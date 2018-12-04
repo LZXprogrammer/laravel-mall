fc342e4eecb2701112ca46cae6ed8d2191026313
lzxprogrammer <lzxprogrammer@outlook.com>
2018-12-04 15:33:18 +0800

    测试csrf

--- app/Http/Middleware/EnableCrossRequestMiddleware.php
+++ app/Http/Middleware/EnableCrossRequestMiddleware.php
@@ -13,21 +13,38 @@ class EnableCrossRequestMiddleware
      * @param  \Closure  $next
      * @return mixed
      */
+    // public function handle($request, Closure $next)
+    // {
+    //     $response = $next($request);
+    //     // $origin = $request->server('HTTP_ORIGIN') ? $request->server('HTTP_ORIGIN') : '';
+    //     // $allow_origin = [
+    //     //     '*',
+    //     // ];
+    //     // if (in_array($origin, $allow_origin)) {
+    //         // $response->header('Access-Control-Allow-Origin', $origin);
+    //         $response->header('Access-Control-Allow-Origin', '*');
+    //         $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, X-CSRF-TOKEN, X-Requested-With, Accept, Authorization, X-XSRF-TOKEN');
+    //         //$response->header('Access-Control-Expose-Headers', 'Authorization, authenticated');
+    //         $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT');
+    //         $response->header('Access-Control-Allow-Credentials', 'false');
+    //     // }
+    //     return $next($request);
+    // }
+
     public function handle($request, Closure $next)
     {
         $response = $next($request);
-        // $origin = $request->server('HTTP_ORIGIN') ? $request->server('HTTP_ORIGIN') : '';
-        // $allow_origin = [
-        //     '*',
-        // ];
-        // if (in_array($origin, $allow_origin)) {
-            // $response->header('Access-Control-Allow-Origin', $origin);
-            $response->header('Access-Control-Allow-Origin', '*');
-            $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, X-CSRF-TOKEN, X-Requested-With, Accept, Authorization, X-XSRF-TOKEN');
-            //$response->header('Access-Control-Expose-Headers', 'Authorization, authenticated');
-            $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT');
-            $response->header('Access-Control-Allow-Credentials', 'false');
-        // }
-        return $next($request);
+        $response->header('Access-Control-Allow-Headers', 'application/x-www-form-urlencoded, Authorization, Accept');
+        // response allow return Authorization header
+        $response->header('Access-Control-Expose-Headers', 'Authorization');
+        $response->header('Access-Control-Allow-Origin', '*');
+        $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+        $response->header('Access-Control-Max-Age', '3628800');
+
+        if ($request->server('REQUEST_METHOD') == 'OPTIONS') {
+            $response->header('status', '200 ok');
+        }
+
+        return $response;
     }
 }
