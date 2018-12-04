34f5b4561a406677a5f69f5babb0ca1f489d9527
lzxprogrammer <lzxprogrammer@outlook.com>
2018-12-04 14:32:08 +0800

    测试csrf

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+use Closure;
 
 class VerifyCsrfToken extends Middleware
 {
@@ -21,4 +22,13 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         //
     ];
+
+    // public function handle($request, Closure $next)
+    // {
+    //     // 使用CSRF
+    //     //return parent::handle($request, $next);
+    //     // 禁用CSRF
+    //     return $next($request);
+    // }
+
 }
