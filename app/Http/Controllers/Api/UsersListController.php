28c8680f67f92fdcd6f3320f3365cbca40152c50
Yaoxin <1002361005@qq.com>
2018-12-07 16:11:58 +0800

    提交会员管理

--- app/Http/Controllers/Api/UsersListController.php
+++ app/Http/Controllers/Api/UsersListController.php
@@ -20,18 +20,18 @@ class UsersListController extends Controller
         $level = $request->get('level');
 
         $page = Config::get('systems.defaultPage');
-
+        $select = ['mobile', 'avatar', 'nick_name', 'create_time', 'is_active', 'active_time', 'promote'];
         $info = array();
         //进行判断是几级分销代理
         switch ($level) {
             case '1':
-                $info = Consumer::where('level_a', session('uid'))->simplePaginate($page);
+                $info = Consumer::where('level_a', session('uid'))->select($select)->simplePaginate($page);
                 break;
             case '2':
-                $info = Consumer::where('level_b', session('uid'))->simplePaginate($page);
+                $info = Consumer::where('level_b', session('uid'))->select($select)->simplePaginate($page);
                 break;
             case '3':
-                $info = Consumer::where('level_c', session('uid'))->simplePaginate($page);
+                $info = Consumer::where('level_c', session('uid'))->select($select)->simplePaginate($page);
                 break;
         }
         return ['code' => '1', 'message' => '请求成功', 'data' => $info];
