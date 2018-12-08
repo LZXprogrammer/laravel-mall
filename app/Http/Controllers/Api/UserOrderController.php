bdd52689a188668f5e72cc09ac6cfb26afd8c898
Yaoxin <1002361005@qq.com>
2018-12-08 10:26:11 +0800

    提交修改

--- app/Http/Controllers/Api/UserOrderController.php
+++ app/Http/Controllers/Api/UserOrderController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Order;
+use App\Models\OrderItem;
+use Illuminate\Support\Facades\DB;
 
 class UserOrderController extends Controller
 {
@@ -20,24 +22,29 @@ class UserOrderController extends Controller
         $info = array();
         switch ($status) {
             case 'all':
-                $info = Order::where('c_id', session('uid'))->with('orderitems')->get();
+                $info = Order::where('c_id', session('uid'))->select('id')->get()->toArray();
                 break;
             case '0':
-                $info = Order::where('c_id', session('uid'))->where('pay_status', $status)->with('orderitems')->get();
+                $info = Order::where('c_id', session('uid'))->where('pay_status', $status)->select('id')->get()->toArray();
                 break;
             case '1':
-                $info = Order::where('c_id', session('uid'))->where('pay_status', $status)->with('orderitems')->get();
+                $info = Order::where('c_id', session('uid'))->where('pay_status', $status)->select('id')->get()->toArray();
                 break;
             case '2':
-                $info = Order::where('c_id', session('uid'))->where('pay_status', $status)->with('orderitems')->get();
+                $info = Order::where('c_id', session('uid'))->where('pay_status', $status)->select('id')->get()->toArray();
                 break;
             case '3':
-                $info = Order::where('c_id', session('uid'))->where('pay_status', $status)->with('orderitems')->get();
+                $info = Order::where('c_id', session('uid'))->where('pay_status', $status)->select('id')->get()->toArray();
                 break;
             case '5':
-                $info = Order::where('c_id', session('uid'))->where('pay_status', $status)->with('orderitems')->get();
+                $info = Order::where('c_id', session('uid'))->where('pay_status', $status)->select('id')->get()->toArray();
                 break;
         }
+        foreach ($info as $k => $v) {
+            $id[] = $v['id'];
+        }
+        $info = DB::table('orders')->whereIn('c_id', session('uid'))->leftJoin('orders.id')->get();
+
         return $info;
     }
 }
